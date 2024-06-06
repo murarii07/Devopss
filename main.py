b74b2860fa19748ded733073756fe94971771331
murarii07 <abhayaj07@gmail.com>
2024-06-06 22:22:12 +0530

    yourfirst commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import *
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/contacts")
+def read_contacts():
+    return [{"name": "John", "age": 30}, {"name": "Jane", "age": 29}]
